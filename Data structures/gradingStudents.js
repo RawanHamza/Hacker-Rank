165458d5a879dfa2758b9a1278a1396951b77753
Rawan El-Ghali <62294577+RawanHamza@users.noreply.github.com>
2022-12-31 01:52:31 +0200

    Update gradingStudents.js

--- Data structures/gradingStudents.js
+++ Data structures/gradingStudents.js
@@ -1,7 +1,7 @@
 function gradingStudents(grades) {
     // Write your code here
     for(let i=0; i<grades.length;i++){
-        if((grades[i]>=38)&&((((grades[i]%5)==3)||((grades[i]%5)==4)))){
+        if((grades[i]>=38)&&((grades[i]%5)>=3)){
             grades[i]=grades[i]+(5-(grades[i]%5));
         }
     }
